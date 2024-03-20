c4ab9c9d7d191219b6c086c16cc8cc8e650dbb67
Asia <asia@Zipcoders-MacBook-Pro-18.local>
2024-03-20 14:55:19 -0400

    progress

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,22 +4,68 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    // instance variable
+    Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        int counterO = 0;
+        int counterX = 0;
+        boolean X = false;
+
+        for(int i = 0; i < this.matrix.length; i++) {
+            for(int j = 0; j < this.matrix.length; j++ ) {
+                if(this.matrix[i][j] == 'X') {
+                    counterX++;
+                } else if (this.matrix[i][j] == 'O') {
+                    counterO++;
+                }
+            }   X = counterX > counterO;
+        }
+        return X;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        int counterO = 0;
+        int counterX = 0;
+        boolean O = false;
+
+        for(int i = 0; i < this.matrix.length; i++) {
+            for(int j = 0; j < this.matrix.length; j++) {
+                if(this.matrix[i][j] == 'O') {
+                    counterO++;
+                } else if (this.matrix[i][j] == 'X') {
+                    counterX++;
+                }
+            }   O = counterO > counterX;
+        }
+        return O;
     }
 
     public Boolean isTie() {
-        return null;
+        int counterO = 0;
+        int counterX = 0;
+        boolean tie = true;
+
+        for(int i = 0; i < this.matrix.length; i++) {
+            for(int j = 0; j < this.matrix.length; j++) {
+                if(this.matrix[i][j] == 'X') {
+                    counterX++;
+                } else if (this.matrix[i][j] == 'O') {
+                    counterO++;
+                }
+            }   tie = counterX == counterO;
+        }
+
+
+        return tie;
     }
 
     public String getWinner() {
+
         return null;
     }
 
