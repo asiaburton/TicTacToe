c48359601ab80cced56783fa2493f7ccdc696bbd
Asia <asia@Zipcoders-MacBook-Pro-18.local>
2024-03-20 18:28:27 -0400

    Finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -25,7 +25,7 @@ public class Board {
                 }
             }   X = counterX > counterO;
         }
-        return X;
+        return getWinner().equals("X");
     }
 
     public Boolean isInFavorOfO() {
@@ -46,27 +46,73 @@ public class Board {
     }
 
     public Boolean isTie() {
-        int counterO = 0;
-        int counterX = 0;
-        boolean tie = true;
-
-        for(int i = 0; i < this.matrix.length; i++) {
-            for(int j = 0; j < this.matrix.length; j++) {
-                if(this.matrix[i][j] == 'X') {
-                    counterX++;
-                } else if (this.matrix[i][j] == 'O') {
-                    counterO++;
-                }
-            }   tie = counterX == counterO;
+        if(getWinner() == "") {
+            return true;
         }
-
-
-        return tie;
+        return false;
     }
 
     public String getWinner() {
+        String winner = "";
+        // check rows
+       String rowWinner = checkRow(matrix);
+        // check columns
+        String columnWinner = checkColumn(matrix);
+        // check diagonal left to right and right to left
+        // 0,0 first corner top left - 1,1 midpoint, 2,2 last point right corner
+        // 0,2 top right corner, 1,1 midpoint, 2,0 bottom left
+        winner = checkDiagonal(matrix);
+      if(columnWinner != "" && isLetter(columnWinner.charAt(0))) {
+            winner = columnWinner;
+       }
+      else if(rowWinner != "" && isLetter(rowWinner.charAt(0))) {
+                winner = rowWinner;
+            }
 
-        return null;
+        return winner;
     }
 
-}
+    private String checkDiagonal(Character[][] matrix) {
+
+        String winner = "";
+        if((matrix[0][0] == matrix[1][1] && matrix[0][0] == matrix[2][2])|| (matrix[0][2] == matrix[1][1] && matrix[1][1] == matrix[2][0])) {
+            winner = String.valueOf(matrix[1][1]);
+        }
+        return winner;
+    }
+
+    public String checkRow(Character[][] matrix) {
+        if(matrix[0][0] == matrix[0][1] && matrix[0][1] == matrix[0][2] && isLetter(matrix[0][0])) {
+           return String.valueOf(matrix[0][0]);
+        }
+        else if(matrix[1][0] == matrix[1][1] && matrix[1][0] == matrix[1][2] && isLetter(matrix[1][0])) {
+            return String.valueOf(matrix[1][0]);
+        }
+        else if(matrix[2][0] == matrix[2][1] && matrix[2][0] == matrix[2][2] && isLetter(matrix[2][0])) {
+            return String.valueOf(matrix[2][0]);
+        }
+        return "";
+
+    }
+
+    public  String checkColumn(Character[][] matrix) {
+            String output = "";
+
+
+        if (matrix[0][0] == matrix[1][0] && matrix[0][0] == matrix[2][0] && isLetter(matrix[0][0])) {
+            return output + matrix[0][0];
+        }
+         else if (matrix[0][1] == matrix[1][1] && matrix[0][1] == matrix[2][1] && isLetter(matrix[0][1])) {
+            return output + matrix[0][1];
+        }
+        else if (matrix[0][2] == matrix[1][2] && matrix[0][2] == matrix[2][2]  && isLetter(matrix[0][2])) {
+            return output + matrix[0][2];
+        }
+        return "";
+    }
+
+    public boolean isLetter(Character character) {
+        return character == 'X' || character == 'O';
+    }
+
+    }
